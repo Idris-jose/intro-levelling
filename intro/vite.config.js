ef9f67211bb00a51072cab6c658809bf02a15db9
Idris jose <idrisjose11@gmail.com>
2025-04-18 18:23:42 +0100

    deleted duplicates

--- intro/vite.config.js
+++ intro/vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: process.env.VITE_BASE_PATH || "/intro-levelling"
+  base: process.env.VITE_BASE_PATH || "/"
 })
